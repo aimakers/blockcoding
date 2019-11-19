6822e0a34132d110f40dbb61e50516d40476a0bb
aimakers <kdkim@everykit.io>
2019-11-19 19:48:58 +0900

    bh1750 조도센서 에러시 -1 리턴

--- blockDriver/blockDriver.js
+++ blockDriver/blockDriver.js
@@ -234,6 +234,9 @@ io.sockets.on('connection', function(socket) {
 					if(!stderr){
 						socket.emit("receiveData",{Type:"ktaimk_get_bh1750_data",Data:{ret:true,data:stdout}});
 					}
+					else{
+						socket.emit("receiveData",{Type:"ktaimk_get_bh1750_data",Data:{ret:true,data:-1}});
+					}
 				});
 			}
 		})
