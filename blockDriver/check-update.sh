13ce42c5f7b3d306b990287a07dbb824d38a0da0
kyudong kim <kdkim@everykit.io>
2019-11-19 15:58:23 +0900

    ssl problem

--- blockDriver/check-update.sh
+++ blockDriver/check-update.sh
@@ -4,7 +4,7 @@ whoami
 
 echo 최신 업데이트 코드를 받습니다.
 
-curl -fsSL https://genieblock.kt.co.kr/update/update.sh -o update.sh && chmod +x ./update.sh && ./update.sh
+curl -kfsSL https://genieblock.kt.co.kr/update/update.sh -o update.sh && chmod +x ./update.sh && ./update.sh
 
 echo 업데이트를 종료합니다. 아무키나 입력해 주세요
 read var
